797022498c05e71ca9149a80f1bb9ae3f1da8992
Josh Yorko <joshua.yorko@gmail.com>
2024-08-08 14:05:17 -0400

    chore: Update nessie-values.yaml to remove unused image and service configurations

--- scripts/python_go_backups/company_creator.go
+++ scripts/python_go_backups/company_creator.go
@@ -99,7 +99,7 @@ func main() {
 		company := SoftwareCompany{
 			Name:      name,
 			Industry:  fake.Industry(),
-			Employees: int32(rand.Intn(20000)),
+			Employees: int32(rand.Intn(200000)),
 			Revenue:   int64(rand.Intn(1000000000)),
 			Location:  fake.City(),
 		}
