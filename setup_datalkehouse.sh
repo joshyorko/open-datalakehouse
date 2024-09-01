f41b144fae4bead24ff425a38434ac135db8d7ad
joshyorko <joshua.yorko@gmail.com>
2024-09-01 10:53:50 -0400

    updated for minikube

--- setup_datalkehouse.sh
+++ setup_datalkehouse.sh
@@ -116,12 +116,12 @@ else
 fi
 
 # Set up Longhorn for storage
-print_status "${YELLOW}" "⏳ Setting up Longhorn for storage..."
-kubectl create ns longhorn-system
-helm repo add longhorn https://charts.longhorn.io
-helm repo update
-helm install longhorn longhorn/longhorn --namespace longhorn-system
-check_pods_ready "longhorn-system"
+#print_status "${YELLOW}" "⏳ Setting up Longhorn for storage..."
+#kubectl create ns longhorn-system
+#helm repo add longhorn https://charts.longhorn.io
+#helm repo update
+#helm install longhorn longhorn/longhorn --namespace longhorn-system
+#check_pods_ready "longhorn-system"
 
 # Set up ArgoCD
 print_status "${YELLOW}" "⏳ Setting up ArgoCD..."
